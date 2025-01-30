645ae94b7e23d40675e0dd0afe02524204310b2f
Eliane Orlandin <liorlandin33@gmail.com>
2025-01-30 19:05:04 -0300

    Float and Double

---
+++ prog0207.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float raio, perimetro;
+    double Pi = 3.1415927, area;
+
+    printf("Digite o raio da circunferência:");
+    scanf("%f", &raio);
+    area = Pi * raio;
+    perimetro = 2 * Pi * raio;
+
+    printf("Área = %f\nPerímetro = %f\n", area, perimetro);
+}
