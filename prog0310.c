d0cd5f663638575799b298df3ca089df8146db15
Eliane Orlandin <liorlandin33@gmail.com>
2025-02-02 11:04:54 -0300

    Ternary operator

--- prog0310.c
+++ prog0310.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+#include <stdio.h>
+
 int main()
+{
+    float salario;
+
+    printf("Qual o salário: \n");
+    scanf("%f", &salario);
+    salario = salario > 1000 ? salario * 1.05 : salario * 1.07;
+    printf("Novo salário: %.2f\n", salario);
+}
+
+/*int main()
 {
     float salario;
 
@@ -16,4 +28,4 @@ int main()
         salario = salario * 1.07;
     }
     printf("Novo salário: %.2f\n", salario);
-}
+}*/
