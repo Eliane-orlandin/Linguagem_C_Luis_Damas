26e87d36b13bdd4b4432385f5b591d97e8e895bb
Eliane Orlandin <liorlandin33@gmail.com>
2025-02-05 10:12:54 -0300

    For

---
+++ prog0412.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    for (i = 1; i <= 100; i = i + 1)
+        if (i == 60)
+            break;
+        else if (i % 2 == 1)
+            continue;
+        else
+            printf("%2d\n", i);
+    printf("FIM DO LAÇO\n");
+}
