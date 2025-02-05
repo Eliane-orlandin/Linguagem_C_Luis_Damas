5921ddf16a175520f2164abee57703187aff8508
Eliane Orlandin <liorlandin33@gmail.com>
2025-02-05 09:39:06 -0300

    For

---
+++ prog0410.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+int main()
+{
+    int i;
+    for (i = 1; i <= 10; i = i + 1)
+        printf("%2d\n", 2 * i);
+}*/
+
+int main()
+{
+    int i;
+    for (i = 2; i <= 20; i = i + 2)
+        printf("%2d\n", i);
+}
