71c0d6e7f7d3c10765582c9973d7312a1e11b775
Eliane Orlandin <liorlandin33@gmail.com>
2025-03-08 21:03:00 -0300

    Congratulations women

---
+++ prog0526.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void coracao()
+{
+    printf("      ***     ***      \n");
+    printf("    ******* *******    \n");
+    printf("   *****************   \n");
+    printf("    ***************    \n");
+    printf("      ***********      \n");
+    printf("        *******        \n");
+    printf("         *****         \n");
+    printf("          ***          \n");
+    printf("           *           \n");
+}
+
+int main()
+{
+    printf("\n Feliz Dia das Mulheres!\n\n");
+    coracao();
+    printf("\nQue cada mulher continue a conquistar\n"
+           "seus sonhos e inspirar futuras gerações");
+    return 0;
+}
