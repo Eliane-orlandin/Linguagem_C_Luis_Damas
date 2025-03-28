9cd272735e593e654648f51c90a9bf7f1e960fff
Eliane Orlandin <liorlandin33@gmail.com>
2025-03-28 10:04:19 -0300

    String

--- prog0705.c
+++ prog0705.c
@@ -7,8 +7,14 @@ int main () {
     char Nome [DIM + 1], Sobrenome [DIM + 1], Completo [2 * DIM + 1];
     while (1) {
         printf("Nome: "); fgets(Nome, sizeof(Nome), stdin);
-        if (strlen(Nome) == 0) break;
-        printf("Sobrenome: "); fgets(Sobrenome, sizeof(Sobrenome), stdin);
+        Nome[strcspn(Nome, "\n")] = '\0';
+
+        if (Nome[0] == '\0') break;
+
+        printf("Sobrenome: "); 
+        fgets(Sobrenome, sizeof(Sobrenome), stdin);
+        Sobrenome[strcspn(Sobrenome, "\n")] = '\0';
+        
         strcpy(Completo, Sobrenome);
         strcat(Completo, SEP_NOME);
         strcat(Completo, Nome);
